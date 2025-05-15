807d489a8322d618dbb940a681dce4fe5bb49bc8
inessuhonjic <ines.suhonjic@hotmail.se>
2025-05-15 12:02:22 +0200

    lite trubbel med styles.css filen

--- vite-test/src/main.jsx
+++ vite-test/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import '../src/';
 import Testing from './components/myFirstComponen.jsx'
 import ExamplePopover from './components/Popover.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
